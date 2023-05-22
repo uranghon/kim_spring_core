6c06432db1e34c2c3c1b11857fbc0ceec8ad3a4d
uranghon <wprkffid2tp@gmail.com>
2023-05-22 19:34:53 +0900

    김영한 스프링 기본편

--- src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -1,50 +1,44 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
+import javax.inject.Provider;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class SingletonWithPrototypeTest2 {
-    @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+public class PrototypeProviderTest {
 
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
-
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-//        assertThat(count2).isEqualTo(1);
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
-
-    @Scope("singleton")
     static class ClientBean {
-
-        // 무식하게 ApplicationContext 를 받아오는 방식으로 하면
-        // 매번 logic 을 실행할 때마다 PrototypeBean 생성해서 쓸 수 있다.
-        // 하지만 지저분하다.
 //        @Autowired
-//        private ApplicationContext applicationContext;
-        private final PrototypeBean prototypeBean;
+//        private ApplicationContext ac;
 
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-//            PrototypeBean bean = applicationContext.getBean(PrototypeBean.class);
-//            bean.addCount();
-//            int count = bean.getCount();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -54,23 +48,20 @@ public class SingletonWithPrototypeTest2 {
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
-
         public void addCount() {
             count++;
         }
-
         public int getCount() {
             return count;
         }
-
         @PostConstruct
         public void init() {
             System.out.println("PrototypeBean.init " + this);
         }
-
         @PreDestroy
         public void destroy() {
-            System.out.println("PrototypeBean.destroy " + this);
+            System.out.println("PrototypeBean.destroy");
         }
     }
+
 }
